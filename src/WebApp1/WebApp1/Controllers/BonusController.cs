986914b61378f088a170d490e6a319b9663b22a5
Oran Halberthal <ohalberthal@intelliflo.com>
2018-11-29 16:25:38 +0000

    Initial commit

---
+++ src/WebApp1/WebApp1/Controllers/BonusController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using WebApp1.DataContracts;
+using WebApp1.Models;
+
+namespace WebApp1.Controllers
+{
+    public class BonusController : Controller
+    {
+        // GET: Bonus
+        public ActionResult Index()
+        {
+            return View("View");
+        }
+
+        // POST: Bonus/Allocate
+        [HttpPost]
+        public async Task<ActionResult> Allocate(BonusViewModel bonusAllocation)
+        {
+            try
+            {
+                var employees = await GetEmployees();
+                List<Employee> recipients = new List<Employee>();
+
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    if (i % bonusAllocation.OneInXEmployees == 0)
+                    {
+                        var recipient = employees[i];
+                        recipients.Add(recipient);
+                        employees.Remove(recipient);
+                    }
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View("View");
+            }
+        }
+
+        private async Task<List<Employee>> GetEmployees()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:57652/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync($"api/employee");
+            if (response.IsSuccessStatusCode)
+            {
+                string employeeData = await response.Content.ReadAsStringAsync();
+                List<Employee> employees = JsonConvert.DeserializeObject<List<Employee>>(employeeData);
+
+                return employees;
+            }
+
+            return new List<Employee>();
+        }
+    }
+}
---
+++ src/WebSvc1/WebSvc1/Models/BonusRecipients.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSvc1.Models
+{
+    public class BonusRecipients
+    {
+        public decimal BonusAmount { get; set; }
+
+        public IEnumerable<Employee> Recipients { get; set; }
+    }
+}
